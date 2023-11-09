421657017b60f85f12f9fe50bdf03798ef19a3aa
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-09 16:26:09 +0600

    service updated successfully

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ const port = process.env.PORT || 5003;
 // middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      // "http://localhost:5174",
+      // "https://b8a11-client-print-hub-finder.web.app/",
+      // "https://b8a11-client-print-hub-finder.firebaseapp.com/",
+      // "https://b8a11-client-print-hub-finder.web.app/",
+      // "https://b8a11-client-print-hub-finder.firebaseapp.com/",
+    ],
     credentials: true,
+    // optionSuccessStatus: 200,
   })
 );
 app.use(express.json());
@@ -38,13 +46,13 @@ const verifyToken = async (req, res, next) => {
   const token = req.cookies?.token;
   console.log("Token in the verifyToken middleware", token);
   if (!token) {
-    return res.status(401).send({ message: "unauthenticated" });
+    return res.status(401).send({ message: "unauthenticateddd" });
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     // error
     if (err) {
       console.log(err);
-      return res.status(401).send({ message: "not authenticated" });
+      return res.status(401).send({ message: "not authenticateddd" });
     }
     // if token is valid then it would be decoded
     console.log("value in the token", decoded);
@@ -261,6 +269,16 @@ async function run() {
       const cursor = serviceCollection.find({ email: req.query?.email });
       const services = await cursor.toArray();
       res.status(200).send(services);
+    });
+
+    app.get("/my-services/update/:id", logger, async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const service = await serviceCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      console.log(service);
+      res.send(service);
     });
 
     app.put(
